6b68e60e5c452d676df0461755dc1970e1bc75d6
mjlee <minjun0124@gmail.com>
2021-06-16 17:01:32 +0900

    feat: safe-food kick-off

---
+++ src/test/java/com/safefood/SafeFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
